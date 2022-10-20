2a64dc8adde0af92c93cd43d7a9bf3872c210417
Caio Figueirôa <cfigueiroa@gmail.com>
2022-10-19 22:14:10 -0300

    make cards click logic and end game

--- js/script.js
+++ js/script.js
@@ -1,25 +1,88 @@
-const minCards = 4;
-const maxCards = 14;
-const parrots = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn'];
+const minCards = 4, maxCards = 14, finished = [], parrots = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn'];
+let pairs, clicks = 0, temp = [], tempcard = [];
+let flagged = false;
 
 function main() {
-    const input = getValidInput(minCards, maxCards);
+    distributeCards();
+    clickWatcher();
+}
+
+function clickWatcher() {
+    const card = document.querySelectorAll(".card");
+    for (const element of card) {
+        element.addEventListener('click', function () {
+            onClick(element);
+        });
+    }
+}
+
+function turn(card) {
+    card.children[1].classList.toggle("back-face");
+}
+
+function turn2(card) {
+    card[0].children[1].classList.toggle("back-face");
+}
+
+function timeout(ele, tempc) {
+    const second = 1000;
+    flagged = true;
+    setTimeout(function () {
+        turn(ele);
+        turn2(tempc);
+        flagged = false;
+    }, second);
+}
+
+function onClick(el) {
+    if (flagged) {
+        return;
+    }
+    const currentCardType = el.children[0].children[0].alt;
+    if (finished.includes(currentCardType)) {
+        return;
+    } else if (temp.length === 0) {
+        temp.push(currentCardType);
+        turn(el);
+        tempcard.push(el);
+        clicks++;
+    } else if (currentCardType !== temp[0]) {
+        turn(el);
+        timeout(el, tempcard);
+        tempcard = [];
+        temp = [];
+        clicks++;
+    } else if (currentCardType === temp[0] && (el !== tempcard[0])) {
+        turn(el);
+        finished.push(currentCardType);
+        tempcard = [];
+        temp = [];
+        clicks++;
+    }
+    if (pairs === finished.length) {
+        alert(`Você ganhou em ${clicks} jogadas!`);
+    }
+}
+
+function distributeCards() {
     const pair = 2;
+    const input = getValidInput(minCards, maxCards);
     const cards = document.querySelector(".cards");
-    let sorteio = getRandom(parrots, input / pair);
+    pairs = input / pair;
+    let randomize = getRandom(parrots, pairs);
     for (let i = 0; i < pair; i++) {
-        for (const element of sorteio) {
+        for (const element of randomize) {
             cards.innerHTML += `<div class="card">
             <div class="front-face face">
               <img src="./img/${element}parrot.gif" alt="${element}" />
             </div>
-            <div class="back-face face">
+            <div class="face">
               <img src="./img/back.png" alt="back" />
             </div>
           </div>`;
         }
         if (i === 0) {
-            sorteio = getRandom(sorteio, sorteio.length);
+            randomize = getRandom(randomize, randomize.length);
         }
     }
 }
@@ -55,4 +118,3 @@ function getRandom(arr, n) {
 }
 
 main();
-
